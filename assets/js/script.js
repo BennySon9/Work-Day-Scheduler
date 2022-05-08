267f68fc2645a460dc476cd6b6d51f336a52bb4c
Brandon Benson <brandonbenson09@gmail.com>
2022-05-08 12:46:36 -0400

    events typed and button pushed are now saved to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
+// saveBtn variable
 var saveBtn = $(".saveBtn");
 
 // displays current calendar day
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+function timeBlockColor() {
+  var hour = moment().hours();
+
+  $(".time-block").each(function () {
+    var currHour = parseInt($(this).attr("id"));
+
+    if (currHour > hour) {
+      $(this).addClass("future");
+    } else if ((currHour = hour)) {
+      $(this).addClass("present");
+    } else if (currHour < hour) {
+      $(this).addClass("past");
+    }
+  });
+}
+
+saveBtn.on("click", function () {
+  var time = $(this).siblings(".hour").text();
+  var plan = $(this).siblings(".plan").val();
+
+  localStorage.setItem(time, plan);
+});
+
+timeBlockColor();
