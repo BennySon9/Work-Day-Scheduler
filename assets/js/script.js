6a4aee1b10be50fe5be7d5bc8838a781e1976212
Brandon Benson <brandonbenson09@gmail.com>
2022-05-08 22:05:44 -0400

    adding more js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ function timeBlockColor() {
 
   $(".time-block").each(function () {
     var currHour = parseInt($(this).attr("id"));
+    // console.log(this); each time-block
 
     if (currHour > hour) {
       $(this).addClass("future");
@@ -20,11 +21,29 @@ function timeBlockColor() {
   });
 }
 
+// clicking the button will save to local for that time block
 saveBtn.on("click", function () {
+  // console.log(this); save button
   var time = $(this).siblings(".hour").text();
   var plan = $(this).siblings(".plan").val();
 
+  // saves the txt time and plan to local storage
   localStorage.setItem(time, plan);
 });
 
+function scheduler() {
+  $(".hour").each(function () {
+    var currHour = $(this).text();
+    var currPlan = localStorage.getItem(currHour);
+    // console.log(this);
+    // console.log(currHour);
+
+    if (currPlan !== null) {
+      $(this).siblings(".plan").val(currPlan);
+    }
+  });
+}
+
+// call functions
 timeBlockColor();
+scheduler();
